271d2e708b0dc0ae75845bb7459a6007bb3e8cf2
Dawood Shahid <dawoodshahid67@gmail.com>
2020-10-17 23:16:09 +0500

    Updates

---
+++ precache-manifest.827851e4cc57969145057a7585211c25.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e75994432c3237c70717e2a39002939",
+    "url": "/Contact-Manager/index.html"
+  },
+  {
+    "revision": "da3a1e9d9b67adbd3035",
+    "url": "/Contact-Manager/static/css/main.5bf8bbee.chunk.css"
+  },
+  {
+    "revision": "14df2715137fc8c00f37",
+    "url": "/Contact-Manager/static/js/2.3896204a.chunk.js"
+  },
+  {
+    "revision": "f9e7e68e90600aabdd7d1f759413461d",
+    "url": "/Contact-Manager/static/js/2.3896204a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "da3a1e9d9b67adbd3035",
+    "url": "/Contact-Manager/static/js/main.abc4af3f.chunk.js"
+  },
+  {
+    "revision": "37bb80b467d4f5a9757e",
+    "url": "/Contact-Manager/static/js/runtime-main.2ad7a173.js"
+  }
+]);
